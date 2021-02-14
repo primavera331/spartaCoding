fa133164f59ddd5f6db452ee17981f6116daca06
primavera331 <79048528+primavera331@users.noreply.github.com>
2021-02-15 00:01:16 +0900

    Add files via upload

---
+++ line-task.py
@@ -0,0 +1,43 @@
+# 1번 문제
+
+from collections import deque
+
+c = 11
+b = 2
+
+
+def catch_me(cony_loc, brown_loc):
+    time = 0
+    queue = deque()
+    queue.append((brown_loc, 0))
+    visited = [{} for _ in range(200001)]
+
+    while cony_loc < 200000:
+        cony_loc += time
+        if time in visited[cony_loc]:
+            return time
+
+        for i in range(0, len(queue)):
+            current_position, current_time = queue.popleft()
+
+            new_position = current_position - 1
+            new_time = current_time + 1
+            if new_position >= 0 and new_time not in visited[new_position]:
+                visited[new_position][new_time] = True
+                queue.append((new_position, new_time))
+
+            new_position = current_position + 1
+            if new_position < 200001 and new_time not in visited[new_position]:
+                visited[new_position][new_time] = True
+                queue.append((new_position, new_time))
+
+            new_position = current_position * 2
+            if new_position < 200001 and new_time not in visited[new_position]:
+                visited[new_position][new_time] = True
+                queue.append((new_position, new_time))
+
+        time += 1
+
+
+print(catch_me(c, b))
+
